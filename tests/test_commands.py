b2a2175ae81ac77573d61519478797855f4d0b60
Iblis Lin <e196819@hotmail.com>
2015-08-06 21:21:05 +0800

    Fix for python2.7.10

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -16,13 +16,13 @@ class CommandsTestCase(unittest.TestCase):
     @patch('couchapp.commands.document')
     def test_init_dest(self, mock_doc):
         commands.init(None, None, '/tmp/mk')
-        mock_doc.assert_called_once_with('/tmp/mk', True)
+        mock_doc.assert_called_once_with('/tmp/mk', create=True)
 
     @patch('os.getcwd', return_value='/mock_dir')
     @patch('couchapp.commands.document')
     def test_init_dest_auto(self, mock_doc, mock_cwd):
         commands.init(None, None)
-        mock_doc.assert_called_once_with('/mock_dir', True)
+        mock_doc.assert_called_once_with('/mock_dir', create=True)
 
 
 if __name__ == '__main__':
