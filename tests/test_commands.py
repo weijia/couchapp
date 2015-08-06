d855f6c26efdcfc944a95a84dd14c1955282bfa9
Iblis Lin <e196819@hotmail.com>
2015-08-07 00:08:27 +0800

    Testing without unittest2

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
 
-import unittest2 as unittest
-
 from couchapp import commands
+from couchapp.errors import AppError
+
 from mock import Mock, patch
+from nose.tools import raises
 
 
-class CommandsTestCase(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    @patch('couchapp.commands.document')
-    def test_init_dest(self, mock_doc):
-        commands.init(None, None, '/tmp/mk')
-        mock_doc.assert_called_once_with('/tmp/mk', create=True)
-
-    @patch('os.getcwd', return_value='/mock_dir')
-    @patch('couchapp.commands.document')
-    def test_init_dest_auto(self, mock_doc, mock_cwd):
-        commands.init(None, None)
-        mock_doc.assert_called_once_with('/mock_dir', create=True)
+@patch('couchapp.commands.document')
+def test_init_dest(mock_doc):
+    commands.init(None, None, '/tmp/mk')
+    mock_doc.assert_called_once_with('/tmp/mk', create=True)
 
 
-if __name__ == '__main__':
-    unittest.main()
+@patch('os.getcwd', return_value='/mock_dir')
+@patch('couchapp.commands.document')
+def test_init_dest_auto(mock_doc, mock_cwd):
+    commands.init(None, None)
+    mock_doc.assert_called_once_with('/mock_dir', create=True)
+
+
+@raises(AppError)
+@patch('os.getcwd', return_value=None)
+@patch('couchapp.commands.document')
+def test_init_dest_auto(mock_doc, mock_cwd):
+    commands.init(None, None)
