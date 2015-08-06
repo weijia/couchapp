91d543f6c4d0a5af4ff9ec5f37b09c81453dc2be
Iblis Lin <e196819@hotmail.com>
2015-08-06 19:33:07 +0800

    Testing for commands.init

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -3,6 +3,7 @@
 import unittest2 as unittest
 
 from couchapp import commands
+from mock import Mock, patch
 
 
 class CommandsTestCase(unittest.TestCase):
@@ -12,8 +13,16 @@ class CommandsTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_init_dest(self):
-        pass
+    @patch('couchapp.commands.document')
+    def test_init_dest(self, mock_doc):
+        commands.init(None, None, '/tmp/mk')
+        mock_doc.assert_called_once_with('/tmp/mk', True)
+
+    @patch('os.getcwd', return_value='/mock_dir')
+    @patch('couchapp.commands.document')
+    def test_init_dest_auto(self, mock_doc, mock_cwd):
+        commands.init(None, None)
+        mock_doc.assert_called_once_with('/mock_dir', True)
 
 
 if __name__ == '__main__':
