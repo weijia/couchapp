f1d54c3b61f26c36818626eb150cc44058a18fed
Iblis Lin <e196819@hotmail.com>
2015-09-24 14:06:38 +0800

    Merge test_utils.py and test_util.py
    Maybe the `test_utils.py` is mis-named, but i cannot recall it.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from couchapp.util import rcpath
+from couchapp.util import discover_apps, iscouchapp, rcpath
 
 from mock import patch
 
@@ -50,3 +50,39 @@ def test_rcpath_env(user_rcpath, isdir, listdir):
 
     assert rcpath() == ['/mock/couchapp.conf', '/tmp/fake.conf'], rcpath()
     assert not user_rcpath.called
+
+
+@patch('couchapp.util.os.path.isfile', return_value=True)
+def test_iscouchapp(isfile):
+    assert iscouchapp('/mock_dir') == True
+    isfile.assert_called_with('/mock_dir/.couchapprc')
+
+
+@patch('couchapp.util.os.listdir', return_value=['foo'])
+@patch('couchapp.util.os.path.isdir', return_value=True)
+@patch('couchapp.util.iscouchapp', return_value=True)
+def test_discover_apps(iscouchapp_, isdir, listdir):
+    assert discover_apps('/mock_dir') == ['/mock_dir/foo']
+    isdir.assert_called_with('/mock_dir/foo')
+    listdir.assert_called_with('/mock_dir')
+
+
+@patch('couchapp.util.os.listdir', return_value=['foo'])
+@patch('couchapp.util.os.path.isdir', return_value=True)
+@patch('couchapp.util.iscouchapp', return_value=True)
+def test_discover_apps_relative_path(iscouchapp_, isdir, listdir):
+    assert discover_apps('mock_dir') == ['mock_dir/foo']
+    isdir.assert_called_with('mock_dir/foo')
+    listdir.assert_called_with('mock_dir')
+
+
+@patch('couchapp.util.os.listdir', return_value=['.git', 'foo'])
+@patch('couchapp.util.os.path.isdir', return_value=True)
+@patch('couchapp.util.iscouchapp', return_value=True)
+def test_discover_apps_hidden_file(iscouchapp_, isdir, listdir):
+    '''
+    Test case for a dir including hidden file
+    '''
+    assert discover_apps('mock_dir') == ['mock_dir/foo']
+    isdir.assert_called_with('mock_dir/foo')
+    listdir.assert_called_with('mock_dir')
